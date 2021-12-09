745675cedb6990f1945bcc6bd8daaba440f70efc
ce4hunnid <christopher_elder1@baylor.edu>
2021-12-09 17:50:58 -0600

    Created Pizzas app and Pizza class

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My APPS
+    'pizzas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
