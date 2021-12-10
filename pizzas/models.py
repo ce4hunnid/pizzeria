e1a6b67f0496748bdabde7f256a266b6a36e51e9
ce4hunnid <christopher_elder1@baylor.edu>
2021-12-09 18:25:53 -0600

    Created Topping Class

--- pizzas/models.py
+++ pizzas/models.py
@@ -8,3 +8,11 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+
+class Topping(models.Model):
+    pizza=models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
