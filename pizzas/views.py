7ed1e64166c54d9706af0c0f2fae1c7eb0184149
ce4hunnid <christopher_elder1@baylor.edu>
2021-12-10 14:51:09 -0600

    Created pizzas.html

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . models import Pizza
 
 # Create your views here.
+def index(request):
+    #The home page for the pizzeria
+    return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.filter(owner=request.user).order_by('date_added')
+    context = {'pizzas':pizzas}
+    return render(request, 'pizzas/pizza.html',context)
